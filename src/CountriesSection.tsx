6856dff310f6a3876ec20c5696a75727edc65de7
maciek367 <maciek2baba2@gmail.com>
2023-02-02 19:16:24 +0100

    implement correct type for response country

--- src/CountriesSection.tsx
+++ src/CountriesSection.tsx
@@ -7,65 +7,50 @@ type Country = {
   name: string;
   nativeName: string;
   capital: string;
-  population: string;
-  region: string;
-  subregion: string;
-  topLevelDomain: string;
+  // population: string;
+  // region: string;
+  // subregion: string;
+  // topLevelDomain: string;
   borderCode: string;
-  borderCountries: string[]
-  flagLink: string;
-  languages: string[]
-  currencies: string[]
+  // borderCountries: string[];
+  // flagLink: string;
+  // languages: string[];
+  // currencies: string[];
 };
 
-type ResponeCountry = {
-  name: {
-    common: string;
-    nativeName: { cat: { official: string } };
-  };
-  capital: string[];
-  population: string;
-  region: string;
-  subregion: string;
-  tld: string[];
-  currencies: { [s: string]: { [s: string]: string } };
-  languages: { [s: string]: string };
-  cca3: string;
-  borders: string[];
-  flags: { [s: string]: string };
+type CountryAllProps = {
+  [key: string]: any;
+};
+
+type ErrorObject = {
+  message: string;
 };
 
 const CountriesSection = () => {
-  const [countries, setCountries] = useState(Array<Country>);
+  const [countries, setCountries] = useState(Array<any>);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const dataRaw = await fetch(FETCH_ALL_COUNTRIES_URI);
-      const data = await dataRaw.json();
-      console.log(data);
-      const modifiedCountries: Country[] = data.map((country: ResponeCountry): Country => {
-        return {
-          name: country.name.common,
-          nativeName: country.name.nativeName.cat.official || "No native name",
-          capital: country.capital ? country.capital[0] : "No capital",
-          population: country.population.toLocaleString(),
-          region: country.region,
-          subregion: country.subregion,
-          topLevelDomain: country.tld[0] || "No top level domain",
-          currencies: Object.values(country.currencies).map(
-            (currency) => currency.name
-          ),
-          languages: Object.values(country.languages),
-          borderCode: country.cca3,
-          borderCountries: country.borders || "No borders",
-          flagLink: country.flags.svg,
-        };
-      });
+    const fetchDataAndSetCountries = async (): Promise<void> => {
+      const countriesAllProps: CountryAllProps[] = await fetch(
+        FETCH_ALL_COUNTRIES_URI
+      )
+        .then((dataRaw) => dataRaw.json())
+        .catch((err: ErrorObject) => console.error(err.message));
 
-      setCountries(modifiedCountries);
+      const clearedCountries: Country[] = countriesAllProps.map(
+        (country: CountryAllProps): Country => {
+          return {
+            name: country.name.common,
+            nativeName: country.name.nativeName,
+            capital: country.capital,
+            borderCode: country.cca3,
+          };
+        }
+      );
+      setCountries(clearedCountries);
     };
 
-    fetchData();
+    fetchDataAndSetCountries();
   }, []);
 
   return (
