c5c64280ca4970ebda1f611ab6866eda71d5205b
maciek367 <maciek2baba2@gmail.com>
2023-02-12 00:23:05 +0100

    add prettier talwindcss classes sorting

--- src/CountryInfo.tsx
+++ src/CountryInfo.tsx
@@ -2,9 +2,10 @@ import { Country } from "./Country";
 
 const CountryInfo = ({ country }: Country) => {
   return (
-    <div className="absolute left-0 top-0 w-full h-full backdrop-blur-md cursor-default">
-      <div className="relative top-[12.5%] left-[12.5%] w-3/4 h-3/4 bg-slate-500">
+    <div className="absolute left-0 top-0 h-full w-full cursor-default backdrop-blur-md">
+      <div className="relative top-[5%] left-[5%] h-[90%] w-[90%] overflow-hidden rounded-xl bg-slate-500">
         <img
+          className=""
           src={country.flagLink}
           alt="country flag"
           onError={(e) => {
@@ -13,6 +14,7 @@ const CountryInfo = ({ country }: Country) => {
             e.currentTarget.src = "/no-image.png";
           }}
         />
+        <div></div>
       </div>
     </div>
   );
