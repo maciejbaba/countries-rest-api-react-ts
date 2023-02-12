522486af4f56bd7fd141359027c641ee29a5ae26
maciek367 <maciek2baba2@gmail.com>
2023-02-12 22:13:37 +0100

    add dark mode button

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,11 +2,14 @@ import { BiWorld } from "react-icons/bi";
 
 const Header = () => {
   return (
-    <header className="flex justify-center p-3">
+    <header className="flex justify-between p-3">
       <div>
         <p className="flex gap-2 p-2 text-2xl">
           <BiWorld size="1.3em" /> Countries
         </p>
+      </div>
+      <div>
+        <button className="p-2 rounded-md shadow-md">Dark Mode</button>
       </div>
     </header>
   );
